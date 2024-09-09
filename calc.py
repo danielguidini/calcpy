fb94ba3ad9e0986dd5f550e5fe15f3994e45b75d
danielguidini <danielguidini2002@gmail.com>
2024-09-08 21:18:33 -0300

    feat: add entrada principal do programa

--- calc.py
+++ calc.py
@@ -1,17 +1,51 @@
 #Metodo Somar
-def somar(x, y):
+def soma(x, y):
     return x + y
 
 #Metodo Subtrair
-def subtrair(x, y):
+def sub(x, y):
     return x - y
 
 #Metodo Multiplicar
-def multiplicar(x, y):
+def mult(x, y):
     return x * y
 
 #Metodo Dividir
-def dividir(x, y):
+def div(x, y):
     if y == 0:
         raise ValueError("Não é possível dividir por zero!")
     return x / y
+
+#Programa Principal
+def calculadora():
+    
+    while True:
+        print("Selecione uma operação:")
+        print("1. Soma")
+        print("2. Subtração")
+        print("3. Multiplicação")
+        print("4. Divisão")
+        print("5. Sair")
+
+        #Escolha da Operação pelo usuario
+        escolha = input("Digite sua escolha: ")
+        
+        #Escape do loop e encerramento do programa
+        if escolha == '5':
+            break
+        
+        #Inserção dos dados pelo o usuario
+        num1 = float(input("Digite o primeiro número: "))
+        num2 = float(input("Digite o segundo número: "))
+
+        #Estrutura Condicional da escolha do usuario
+        if escolha == '1':
+            print("Resultado:", soma(num1, num2))
+        elif escolha == '2':
+            print("Resultado:", sub(num1, num2))
+        elif escolha == '3':
+            print("Resultado:", mult(num1, num2))
+        elif escolha == '4':
+            print("Resultado:", div(num1, num2))
+        else:
+            print("Escolha inválida. Tente novamente.")
